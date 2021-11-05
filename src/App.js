d4531df6d4ee82b84e07ca84b2b03ffd49f09e7d
WincyLiu <87019971+WincyLiu@users.noreply.github.com>
2021-11-05 16:54:46 +0000

    Updated footer and added default city

--- src/App.js
+++ src/App.js
@@ -3,18 +3,26 @@ import Weather from "./Weather";
 
 function App() {
   return (
-    <div className="App">
-    <div className="container">
-      <Weather />
-       <footer className="contact">
-        <a
-          href="https://github.com/WincyLiu/ReactJS-Weather"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-Source Code
-        </a>
-        <span> By Wincy Liu </span>
+   <div className="App">
+      <div className="container">
+        <Weather defaultCity="London" />
+        <footer className="contact">
+          This project was coded by {""}
+          <a
+            href="https://stupefied-snyder-c092d0.netlify.app/index.html#"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Wincy Liu
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/WincyLiu/ReactJS-Weather"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-Source Code
+          </a>
       </footer>
     </div>
     </div>
