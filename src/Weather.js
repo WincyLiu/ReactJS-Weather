8b1c11c6ace355d706be3bad44f2aaa5d45bef61
WincyLiu <87019971+WincyLiu@users.noreply.github.com>
2021-11-07 18:52:50 +0000

    Added Weather Forecast component

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import FormattedDate from "./FormattedDate";
 import WeatherInfo from "./WeatherInfo";
+import WeatherForecast from "./WeatherForecast";
 import axios from "axios";
 import "./Weather.css";
 
@@ -84,6 +85,7 @@ export default function Weather(props) {
           </div>
         </form>
         <WeatherInfo data={weatherData} />
+       <WeatherForecast />
       </div>
     );
   } else {
